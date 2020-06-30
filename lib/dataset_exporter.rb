da4a30529849812f5b1acf3bf2d31888c43525f1
Kenneth Leung <kenneth@leungs.us>
2020-06-30 13:56:49 -0700

    handle different types of rows return by a dataset.

--- lib/dataset_exporter.rb
+++ lib/dataset_exporter.rb
@@ -7,39 +7,31 @@ module DatasetExporter
     ds.db
   end
 
-  def columns
-    ds.columns
+  # @return an array of hashes
+  def records
+    @records ||= ds.naked
   end
 
-  def headings
-    @headings ||= begin
-                    case
-                    when first_row.is_a?(Hash)
-                      first_row.keys
-                    when first_row.kind_of?(Sequel::Model)
-                      first_row.values.keys
-                    else
-                      raise "#{first_row.class} not defined"
-                    end
-                  end
+  def columns
+    @columns ||= records.first.keys
   end
+
+  alias_method :headings, :columns
+
+  # def headings
+  #   @headings ||= begin
+  #                   case
+  #                   when first_row.is_a?(Hash)
+  #                     first_row.keys
+  #                   when first_row.kind_of?(Sequel::Model)
+  #                     first_row.values.keys
+  #                   else
+  #                     raise "#{first_row.class} not defined"
+  #                   end
+  #                 end
+  # end
 
   def rows
-    @rows ||= begin
-                case
-                when first_row.is_a?(Hash)
-                  ds.all.map(&:values)
-                when first_row.kind_of?(Sequel::Model)
-                  ds.all.map(&:values).map(&:values)
-                else
-                  raise "#{first_row.class} not defined"
-                end
-              end
-  end
-
-  private
-
-  def first_row
-    @first_row ||= ds.first
+    @rows ||= records.map { |hsh| hsh.values }
   end
 end

b433dc4a60b10f5640cdc039f860d8e8d1d9618f
lkfken <lkfken@hotmail.com>
2022-10-10 15:09:12 -0700

    Initial commit to a new branch 1.0.0

--- dataset_exporter.gemspec
+++ dataset_exporter.gemspec
@@ -22,5 +22,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec"
   spec.add_development_dependency "os"
+  spec.add_development_dependency "solargraph"
   spec.add_dependency "caxlsx"
 end
--- lib/dataset_exporter/excel.rb
+++ lib/dataset_exporter/excel.rb
@@ -1,20 +1,24 @@
-require "axlsx"
-require "pathname"
+require 'axlsx'
+require 'pathname'
 
 module DatasetExporter
   class Excel
     include DatasetExporter
-    attr_reader :ds, :filename, :headers, :workbook, :package, :types
+    attr_reader :filename, :headers, :package, :sheets
 
-    def initialize(params = {})
-      @filename = params.fetch(:filename, "default.xlsx")
-      @ds = params.fetch(:ds)
-      @headers = params.fetch(:headers, true) # true => include headers
-      @types = params.fetch(:types, _types) #type must be one of [:date, :time, :float, :integer, :string, :boolean, :iso_8601]
-      @package = Axlsx::Package.new
-      @workbook = @package.workbook
-      raise DatasetError, ["@ds has no records!", @ds.inspect].join($/) if @ds.empty?
-      add_rows
+    # sheets = {name[String]: dataset[Sequel::Dataset]}
+    def initialize(sheets: {}, filename: 'default.xlsx', headers: true,
+                   package: Axlsx::Package.new)
+      @filename = filename
+      @sheets = sheets
+      @headers = headers
+      @package = package
+
+      add_sheets
+    end
+
+    def workbook
+      @workbook ||= package.workbook
     end
 
     def to_str
@@ -26,18 +30,16 @@ module DatasetExporter
     end
 
     def to_file(filename: self.filename)
-      begin
-        package.serialize(filename)
-      rescue => ex
-        File.delete(filename) if File.exist?(filename)
-        raise ex
-      end
+      package.serialize(filename)
+    rescue StandardError => e
+      File.delete(filename) if File.exist?(filename)
+      raise e
     end
 
     private
 
-    def _types
-      ds_types.map do |type|
+    def try_convert_types(types)
+      types.map do |type|
         case type
         when :bigdecimal, :float
           :float
@@ -57,14 +59,18 @@ module DatasetExporter
       end
     end
 
-    def ds_types
-      @ds.first.to_hash.values.map { |v| v.class.to_s.downcase.to_sym }
-    end
-
-    def add_rows
-      workbook.add_worksheet do |sheet|
-        sheet.add_row headings if headers
-        rows.each { |row| sheet.add_row row, :types => types }
+    def add_sheets
+      sheets.each do |name, ds|
+        raise DatasetError, ['@ds has no records!', ds.inspect].join($/) if ds.empty?
+        records = ds.naked
+        headings = records.first.keys
+        rows = records.map { |hsh| hsh.values }
+            
+        workbook.add_worksheet(name: name.to_s) do |sheet|
+          sheet.add_row headings if headers
+          types = try_convert_types(ds.first.to_hash.values.map { |v| v.class.to_s.downcase.to_sym })
+          rows.each { |row| sheet.add_row row, types: types}
+        end
       end
     end
   end
